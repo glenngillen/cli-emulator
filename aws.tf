e53757e4865d50f6e57a32a8b803e1e6dab8a3a9
Glenn Gillen <me@glenngillen.com>
2020-03-25 10:25:00 +1100

    Adding initial Terraform config.

---
+++ aws.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  version = "~> 2.17"
+  region = "us-east-1"
+}
---
+++ websocket-server.tf
@@ -0,0 +1,8 @@
+resource "aws_apigatewayv2_api" "cli_websocket_signalling_server" {
+  name                       = "cli_websocket_signalling_server"
+  protocol_type              = "WEBSOCKET"
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.cli_websocket_signalling_server
+}
