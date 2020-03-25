770d938aaca97ae996ad2542675def0b0beb538a
Glenn Gillen <me@glenngillen.com>
2020-03-25 12:47:44 +1100

    Get explict about websocket route selection approach.

--- websocket-server.tf
+++ websocket-server.tf
@@ -1,6 +1,7 @@
 resource "aws_apigatewayv2_api" "cli_websocket_signalling_server" {
   name                       = "cli_websocket_signalling_server"
   protocol_type              = "WEBSOCKET"
+  route_selection_expression = "$request.body.action"
 }
 
 output "api_url" {
