e6ff73eab2731a32de342d4df94308adcb82dabb
Glenn Gillen <me@glenngillen.com>
2020-09-21 15:40:33 +1000

    Style command panel.

--- src/CommandPanel.tsx
+++ src/CommandPanel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Button, Footer, Layer, FormField, TextInput, TextArea, Grid } from "grommet";
 import styled from "styled-components";
+import { theme } from "./Theme";
 
 interface PanelProps {
   gridArea?: string
@@ -17,22 +18,31 @@ interface PanelState {
   cmds: Array<MatchedCommand>
 }
 
+const Cmd = styled(Box)`
+  font-family: ${theme['code-font-family']};
+  font-size: ${theme['font-size']};
+  background: #ddd;
+  color: #333;
+  padding: ${theme['grid-base-spacing']};
+  border-bottom: 1px solid #aaa;
+`
+const Output = styled(Box)`
+  font-family: ${theme['code-font-family']};
+  font-size: ${theme['font-size']};
+  background: #ddd;
+  color: #333;
+  padding: ${theme['grid-base-spacing']};
+  border-bottom: 1px solid #aaa;
+`
 class CommandPanel extends React.Component<PanelProps, PanelState> {
   constructor(props) {
     super(props)
-    let storedCmds = window.localStorage.getItem('cmds')
-    let cmds: Array<MatchedCommand> = []
-    if (storedCmds) {
-      let data = JSON.parse(storedCmds)
-      data.forEach(element => {
-        cmds.push({cmd: element.cmd, output: element.output})
-      })
-    }
-    this.state = { showAdd: false, cmds: cmds }
+    this.state = { showAdd: false, cmds: [] }
     this.showAdd = this.showAdd.bind(this)
     this.hideAdd = this.hideAdd.bind(this)
     this.addCommand = this.addCommand.bind(this)
     this.saveCommands = this.saveCommands.bind(this)
+    this.loadCommands = this.loadCommands.bind(this)
     this.processInput = this.processInput.bind(this)
   }
 
@@ -44,7 +54,20 @@ class CommandPanel extends React.Component<PanelProps, PanelState> {
     this.setState({ showAdd: false })
   }
 
+  loadCommands() {
+    let storedCmds = window.localStorage.getItem('cmds')
+    let cmds: Array<MatchedCommand> = []
+    if (storedCmds) {
+      let data = JSON.parse(storedCmds)
+      data.forEach(element => {
+        cmds.push({cmd: element.cmd, output: element.output})
+      })
+    }
+    this.setState({ cmds: cmds })
+  }
   saveCommands() {
+    let cmds = this.state.cmds
+    window.localStorage.setItem('cmds', JSON.stringify(cmds))
   }
 
   processInput(cmd) {
@@ -57,23 +80,25 @@ class CommandPanel extends React.Component<PanelProps, PanelState> {
   }
 
   componentDidMount() {
+    this.loadCommands()
   }
+
   addCommand() {
     let cmds = this.state.cmds
     if (this.state.newCmd && this.state.newOutput) {
       cmds.push({cmd: this.state.newCmd, output: this.state.newOutput})
     }
-    window.localStorage.setItem('cmds', JSON.stringify(cmds))
     this.setState({
       cmds: cmds,
       newCmd: '',
       newOutput: ''
     })
+    this.saveCommands()
     this.hideAdd()
   }
 
   render() {
-    return <Box gridArea={this.props.gridArea} fill={true}>
+    return <Box gridArea={this.props.gridArea} fill={true} direction="column">
       {this.state.showAdd && (
         <Layer onEsc={this.hideAdd} full={true}>
           <Box pad="medium">
@@ -89,16 +114,18 @@ class CommandPanel extends React.Component<PanelProps, PanelState> {
           </Box>
         </Layer>
       )}
-      {this.state.cmds.map((item, idx) => (
-        <Grid columns={['small','medium']} areas={[['cmd','output']]} gap="xsmall" pad="xsmall">
-          <Box gridArea="cmd">
-            {item.cmd}
-          </Box>
-          <Box gridArea="output">
-            {item.output}
-          </Box>
-        </Grid>
-      ))}
+      <Box flex="grow">
+        {this.state.cmds.map((item, idx) => (
+          <Grid columns={['small','flex']} areas={[['cmd','output']]} gap="none" pad="none" >
+            <Cmd gridArea="cmd">
+              {item.cmd}
+            </Cmd>
+            <Output gridArea="output">
+              {item.output}
+            </Output>
+          </Grid>
+        ))}
+      </Box>
       <Footer pad="small" align="end" direction="row-reverse">
         <Button primary alignSelf="end" label="Add new command" onClick={this.showAdd}/>
       </Footer>
